2e109212feaa7a07bdcec3e2fe80300991322fd6
Joseph Surla <josephsurla@gmail.com>
2016-03-04 10:17:56 -0800

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,9 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.belongs_to :taxi
+      t.belongs_to :passenger
+      
       t.timestamps null: false
     end
   end
