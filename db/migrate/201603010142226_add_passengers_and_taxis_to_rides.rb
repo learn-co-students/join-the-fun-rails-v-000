79eb74510b05533b3051edeef3761db9d03254f9
Alvinluipo <lualvin1234@gmail.com>
2016-03-10 14:08:59 -0500

    Done.

---
+++ db/migrate/201603010142226_add_passengers_and_taxis_to_rides.rb
@@ -0,0 +1,8 @@
+class AddPassengersAndTaxisToRides < ActiveRecord::Migration
+  def change
+    change_table :rides do |t|
+      t.integer :passenger_id
+      t.integer :taxi_id
+    end
+  end
+end
