dbbda7b2977a5f9ddda28749c09bb7bb5af0e821
rocketNeck <homestarflyer@gmail.com>
2016-04-21 16:07:57 -0400

    Done.

---
+++ db/migrate/20160421195827_add_foreign_keys_to_rides.rb
@@ -0,0 +1,6 @@
+class AddForeignKeysToRides < ActiveRecord::Migration
+  def change
+    add_reference :rides, :passenger, index: true, foreign_key: true
+    add_reference :rides, :taxi, index: true, foreign_key: true
+  end
+end
