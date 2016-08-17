c2a7127e083796b6eb965c1944aa84916525045e
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-08-17 23:46:50 +0000

    Done.

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,9 @@
 class Taxi < ActiveRecord::Base
+
+  has_many :rides
+  # Taxi.rides => [all of this taxis ride ]
+
+  has_many :passengers, through: :rides
+  # Taxi.passengers = will list passengers that went in this taxi
+          #  so it when a ride gets added to Taxi rides it will add this rides passenger to this taxis
 end
