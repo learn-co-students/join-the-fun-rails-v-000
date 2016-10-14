84a07e5a82ad794060b91f1c79b2bf58e34dafbd
Samuel Lair <lair001@gmail.com>
2016-10-14 04:22:50 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,6 @@
 class Ride < ActiveRecord::Base
+  has_one :passenger_ride
+  belongs_to :passenger
+  has_one :taxi_ride
+  belongs_to :taxi
 end
---
+++ test/models/taxi_ride_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TaxiRideTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
