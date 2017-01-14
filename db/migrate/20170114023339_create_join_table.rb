26bf9039fb1d18cf9fc40e43fc5a93ec9dbc6a60
christinechoi <smi77y@gmail.com>
2017-01-13 22:01:19 -0500

    Done.

---
+++ db/migrate/20170114023339_create_join_table.rb
@@ -0,0 +1,8 @@
+class CreateJoinTable < ActiveRecord::Migration
+  def change
+    create_join_table :taxis, :passengers do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
+    end
+  end
+end
