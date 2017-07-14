6d23b9105443a2fae23641647bd66e5c0f8c0c0e
curlywallst <nnoyes@comcast.net>
2017-07-14 16:31:15 -0400

    Done.

---
+++ db/migrate/20170714203006_add_ride_id_to_taxi.rb
@@ -0,0 +1,5 @@
+class AddRideIdToTaxi < ActiveRecord::Migration
+  def change
+    add_column :taxis, :ride_id, :integer
+  end
+end
---
+++ db/migrate/20170714203029_add_ride_id_to_passenger.rb
@@ -0,0 +1,5 @@
+class AddRideIdToPassenger < ActiveRecord::Migration
+  def change
+    add_column :passengers, :ride_id, :integer
+  end
+end
