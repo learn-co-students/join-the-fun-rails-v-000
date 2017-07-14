5c5be816e203dfda114b238a0a83003fe97d486c
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-07-14 18:08:46 +0000

    Done.

---
+++ app/controllers/passenger_controller.rb
@@ -0,0 +1,2 @@
+class PassengerController < ApplicationController
+end
---
+++ test/controllers/taxi_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TaxiControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
