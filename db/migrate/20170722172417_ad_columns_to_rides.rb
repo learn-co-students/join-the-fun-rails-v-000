1efcc9226dc302e5a61e0bd93a2c90576ded982f
jessica fletcher <cara.kane@gmail.com>
2017-07-22 17:27:20 +0000

    Done.

--- db/migrate/20170722172417_ad_columns_to_rides.rb
+++ db/migrate/20170722172417_ad_columns_to_rides.rb
@@ -1,4 +1,4 @@
-class AdColumnsToRides < ActiveRecord::Migration
+class AddColumnsToRides < ActiveRecord::Migration
   def change
     add_column :rides, :taxi_id, :integer
     add_column :rides, :passenger_id, :integer
