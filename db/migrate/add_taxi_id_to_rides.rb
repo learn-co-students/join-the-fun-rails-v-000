b4218c3ca6aaf4d03dedbff522356a3b15e378ee
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-01-08 12:23:34 -0500

    Done.

--- db/migrate/add_taxi_id_to_rides.rb
+++
@@ -1,5 +0,0 @@
-class AddTaxiIdToRides < ActiveRecord::Migration
-  def change
-    add_column :rides, :taxi_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "taxi_id"
+    t.integer  "passenger_id"
   end
 
   create_table "taxis", force: :cascade do |t|
