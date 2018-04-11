7d5c88adc3dd8defd98e81b00636ad84a9e6c5fb
Morgan Fudge <mfudge92@gmail.com>
2018-04-11 20:55:08 +0000

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      t.belongs_to :taxi, index: true
+      t.belongs_to :passenger, index: true
     end
   end
 end
