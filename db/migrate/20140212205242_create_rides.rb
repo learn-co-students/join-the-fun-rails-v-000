027ec034b7458d8e75b068019db2332f13d1a8e3
Robert Hughes <roberts.ginger.email@gmail.com>
2018-04-14 22:29:10 +0100

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.belongs_to :taxi
+      t.belongs_to :passenger
       t.timestamps null: false
     end
   end
