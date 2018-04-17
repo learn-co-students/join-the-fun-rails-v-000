ef0e1a36328d260e31e13ec9b50a5d29958d940a
Susan Fowler <infopro94@gmail.com>
2018-04-17 15:13:41 -0500

    created relationships and added foreignkey table still getting errors desperately need a break

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 JoinTheFunRails::Application.routes.draw do
+
+  resources :taxis, :passengers, :rides
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
