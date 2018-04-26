864c063063489a8ad80830baa0922b623a53f213
chaimoosh <chaimoosh@gmail.com>
2018-04-26 18:38:46 -0400

    Done.

---
+++ db/migrate/20180426212706_create_join_table_taxis_passengers.rb
@@ -0,0 +1,10 @@
+class CreateJoinTableTaxisPassengers < ActiveRecord::Migration
+  def change
+    create_join_table :taxis, :passengers do |t|
+      # t.index [:taxi_id, :passenger_id]
+      # t.index [:passenger_id, :taxi_id]
+      t.integer :taxi_id
+      t.integer :passenger_id
+    end
+  end
+end
