c2bebe2b4abb4b40c42c59123dc3bbcb2bfddfac
github username <github email address>
2018-05-23 19:10:34 -0500

    Done.

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,6 +1,4 @@
 class Taxi < ActiveRecord::Base
-    has_many :passengers_rides
+    has_many :rides
     has_many :passengers, through: :rides
-    # has_many :passengers_rides
-    # has_many :passengers, through: :rides
 end
