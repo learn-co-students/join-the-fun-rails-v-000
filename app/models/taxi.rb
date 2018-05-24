bc5624625b9df11556cfb3d1f9730dc4cf99bbfb
Aly Alidina <aly_alidina@hotmail.com>
2018-05-24 21:23:18 +0000

    Done.

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,4 +1,4 @@
 class Taxi < ActiveRecord::Base
-  has_many :rides
-  has_many :passengers, through: :rides
+  has_many :rides #rides is the join table
+  has_many :passengers, through: :rides #taxi is getting many passager through rides joint table
 end
