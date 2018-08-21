c12a0fcc6f1479a0a33b7f50e3ddf39f537ab60a
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-21 02:54:49 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20180821025242) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "taxi_id"
+    t.integer  "passenger_id"
   end
 
   create_table "taxis", force: :cascade do |t|
