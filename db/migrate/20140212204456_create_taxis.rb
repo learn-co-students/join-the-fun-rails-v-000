c035f60783a401ee4ae4b8f6124bdbba8843b27f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-21 02:48:32 +0000

    Done.

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -1,6 +1,8 @@
 class CreateTaxis < ActiveRecord::Migration
   def change
     create_table :taxis do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
       t.timestamps null: false
     end
   end
