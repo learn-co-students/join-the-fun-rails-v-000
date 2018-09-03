5d425b978ff76b73124b6ef152d778ca5f99d42a
Adam Kalt <adam.kalt.1@gmail.com>
2018-09-03 17:52:27 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,4 @@
 class Ride < ActiveRecord::Base
-  belongs_to :passengers
-  belongs_to :taxis
+  belongs_to :passenger
+  belongs_to :taxi
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,4 +1,4 @@
 class Taxi < ActiveRecord::Base
   has_many :rides
-  has_many :passengers, through :rides
+  has_many :passengers, through: :rides
 end
