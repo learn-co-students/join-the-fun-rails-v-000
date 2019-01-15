16af088d35bbd865dd817003fa3ee292a1f0be35
Judy McGoldrick <jjeanius@verizon.net>
2019-01-15 04:38:42 +0000

    Done.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,10 @@
 class Passenger < ActiveRecord::Base
+  has_many :rides
+  has_many :taxis, through: :rides
+
+  def passenger
+    @passenger.taxis << taxi
+    @passenger.rides << ride
+  end
+
 end
