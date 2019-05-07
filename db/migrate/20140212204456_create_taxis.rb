389898a982aed62ba3aa9d84f0ddb8edc1646102
github username <github email address>
2019-05-07 15:08:20 -0400

    Done.

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -1,6 +1,7 @@
 class CreateTaxis < ActiveRecord::Migration
   def change
     create_table :taxis do |t|
+      t.belongs_to :ride_id
       t.timestamps null: false
     end
   end
--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.belongs_to :taxi, index: true
+      t.belongs_to :passenger, index: true
       t.timestamps null: false
     end
   end
