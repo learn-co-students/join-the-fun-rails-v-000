434423bd0d521473a65abdab692d63020dc1e920
Scott Silverman <scottms724@gmail.com>
2019-09-30 14:34:31 +0000

    Done.

---
+++ db/migrate/20190930142817_add_to_rides_passengers_and_taxis.rb
@@ -0,0 +1,8 @@
+class AddToRidesPassengersAndTaxis < ActiveRecord::Migration[5.0]
+  
+  def change
+    add_column :rides, :taxi_id, :integer 
+    add_column :rides, :passenger_id, :integer 
+  end
+  
+end
