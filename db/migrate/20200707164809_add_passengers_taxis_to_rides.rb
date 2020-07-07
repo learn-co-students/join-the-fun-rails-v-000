f32578aaece7bd25b92b76bd59f9ff79ea67ddbd
klovae <elyse.klova@gmail.com>
2020-07-07 12:52:25 -0400

    Done.

---
+++ db/migrate/20200707164809_add_passengers_taxis_to_rides.rb
@@ -0,0 +1,6 @@
+class AddPassengersTaxisToRides < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :rides, :passenger, foreign_key: true
+    add_reference :rides, :taxi, foreign_key: true
+  end
+end
