80d7e04733679607033688d95fca3800bd4e78f1
dejarvzal <diannejarvisj@gmail.com>
2020-07-28 20:27:28 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,4 @@
 class Ride < ActiveRecord::Base
-    has_many :passengers
-    has_many :taxis
+    belongs_to :passenger
+    belongs_to :taxi
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200727171009) do
+ActiveRecord::Schema.define(version: 20200729024546) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20200727171009) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "taxi_id"
+    t.integer  "passenger_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "rides_taxis", id: false, force: :cascade do |t|
