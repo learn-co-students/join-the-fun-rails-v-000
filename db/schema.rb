1a6272f5c8fd3457bafa0d07719d70e5b6be7db3
lrh2daizzo <herrera.lrh@gmail.com>
2021-06-23 18:19:53 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200524174600) do
+ActiveRecord::Schema.define(version: 20210623231752) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20200524174600) do
   create_table "rides", force: :cascade do |t|
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "taxi_id"
     t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
